2dd7113244f0554799c4f3f4de82634f80f31c58
Akshata Jog <136689107+akshatasProjects@users.noreply.github.com>
2024-09-04 18:09:26 +0530

    Add files via upload

---
+++ Day2/ExercisesLevel-1.py
@@ -0,0 +1,43 @@
+"""
+# Inside 30DaysOfPython create a folder called day_2. Inside this folder create a file named variables.py
+# Write a python comment saying 'Day 2: 30 Days of python programming'
+# Declare a first name variable and assign a value to it
+# Declare a last name variable and assign a value to it
+# Declare a full name variable and assign a value to it
+# Declare a country variable and assign a value to it
+# Declare a city variable and assign a value to it
+# Declare an age variable and assign a value to it
+# Declare a year variable and assign a value to it
+# Declare a variable is_married and assign a value to it
+# Declare a variable is_true and assign a value to it
+# Declare a variable is_light_on and assign a value to it
+Declare multiple variable on one line
+"""
+
+# Write a python comment saying 'Day 2: 30 Days of python programming'
+
+# Declare a first name variable and assign a value to it
+first_name = "Akshata"
+#Declare a last name variable and assign a value to it
+last_name = "Ajit"
+#Declare a full name variable and assign a value to it
+full_name = "Akshata Ajit"
+#Declare a country variable and assign a value to it
+country = "India"
+#Declare a city variable and assign a value to it
+city = "Dharwad"
+#Declare an age variable and assign a value to it
+age = 36
+#Declare a year variable and assign a value to it
+year = 2024
+# Declare a variable is_married and assign a value to it
+is_married = True
+# Declare a variable is_true and assign a value to it
+is_true = False
+# Declare a variable is_light_on and assign a value to it
+is_light_on = True
+
+first_name, last_name, city = "Atharva", "Jog", "Dharwad"
+
+#Check the data type of all your variables using type() built-in function
+
