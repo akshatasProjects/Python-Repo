eb5d74f2dad9e2305b37cb9f805212b2b679d123
Akshata Jog <136689107+akshatasProjects@users.noreply.github.com>
2024-09-09 18:01:32 +0530

    Add files via upload

---
+++ Day3/Day4/StringFormatting.py
@@ -0,0 +1,19 @@
+# %s - String
+# %d - Integers
+# %f - Floating point numbers
+# "%.number of digitsf" - Floating point numbers with fixed precision
+
+# first_name = "Akshata"
+# last_name = "Ajit"
+# full_name = first_name +' '+last_name
+# formatted_string = ' I am %s %s. my full name is %s' %(first_name, last_name, full_name)
+# print(formatted_string)
+
+# Strings  and numbers
+radius = 10
+pi = 3.14
+area = pi * radius ** 2
+formated_string = ' The area of circle with radius %d is %.2f' %(radius, area)
+print(formated_string)
+
+
